Fix Hadamard in gradient, add logistic

Assignments/week03/logistic_regression.py
@@ -3,23 +3,32 @@
 import numpy as np
 import random
 
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    return sigmoid(z)/(1.0-sigmoid(z))
+    
 def inference(w, b, X): 
     '''X is vector or matrix of m*n'''
-    Y_pred = np.dot(X, w) + b
+    z = np.dot(X, w) + b
+    Y_pred = sigmoid(z)
     return Y_pred
 
 def eval_loss(w, b, X, gt_Y):
-    return np.linalg.norm(np.dot(X, w) + b - gt_Y) * 0.5
+    return np.linalg.norm(inference(w, b, X) - gt_Y) * 0.5
 
-def gradient(pred_Y, gt_Y, X):
+def gradient(gt_Y, X, w, b):
+    z = np.dot(X, w) + b
+    pred_Y = sigmoid(z)
     diff = pred_Y - gt_Y
-    dw = np.mean((diff * X.T).T, axis=0)
-    db = np.mean(diff)
+    sp = sigmoid_prime(z)
+    dw = np.mean((diff * sp * X.T).T, axis=0)
+    db = np.mean(diff * sp)
     return dw, db
 
 def cal_step_gradient(batch_X, batch_gt_Y, w, b, lr):
-    batch_pred_Y = inference(w, b, batch_X)
-    dw, db = gradient(batch_pred_Y, batch_gt_Y, batch_X)
+    dw, db = gradient(batch_gt_Y, batch_X, w, b)
     w -= lr * dw
     b -= lr * db
     return w, b
@@ -38,17 +47,18 @@ def train(X, gt_Y, batch_size, lr, max_iter):
 
 def gen_sample_data():
     num_samples = 100
-    w = random.randint(0, 10) + random.random()		# for noise random.random[0, 1)
-    b = random.randint(0, 5) + random.random()
-    X = np.random.randn(num_samples) * 100
-    Y = np.dot(X, w) + b + np.random.randn(num_samples) * random.randint(-1, 1)
+    w = random.random()		# for noise random.random[0, 1)
+    b = random.random()
+    X = np.random.randn(num_samples)
+    Y = sigmoid(np.dot(X, w) + b)+ np.random.randn(num_samples) * 0.1
     return X, Y, w, b
     
 def run():
     X, Y, w, b = gen_sample_data()
-    lr = 0.0001
-    max_iter = 10
+    lr = 0.1
+    max_iter = 10000
     train(X, Y, 50, lr, max_iter)
+    print(w, b)
     
     
 if __name__ == '__main__':