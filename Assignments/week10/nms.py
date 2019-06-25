03c6ed08ae5e28e368b53a91dc33b2e7656fb045
leip <ftgh2003@126.com>
2019-06-25 15:02:51 +0800

    exchange x2,y1 in nms.py

--- Assignments/week10/nms.py
+++ Assignments/week10/nms.py
@@ -2,7 +2,7 @@
 Non-Maximum Suppression
 '''
 def NMS(lists, thre):
-    
+    #l[0:4]: x1, x2, y1, y2; l[4]: score
     if not lists:
         print('No region input.')
         return None
@@ -22,27 +22,27 @@ def NMS(lists, thre):
         
 def IOU(reg1, reg2):
     x1 = max(reg1[0], reg2[0])
-    y1 = max(reg1[1], reg2[1])
-    x2 = min(reg1[2], reg2[2])
+    x2 = max(reg1[1], reg2[1])
+    y1 = min(reg1[2], reg2[2])
     y2 = min(reg1[3], reg2[3])
 
     if x2-x1 < 0 or y2-y1 < 0:
         return 0
 
     s_in = (x2-x1) * (y2-y1)
-    s1 = (reg1[2] - reg1[0]) * (reg1[3] - reg1[1])
-    s2 = (reg2[2] - reg2[0]) * (reg2[3] - reg2[1])
+    s1 = (reg1[1] - reg1[0]) * (reg1[3] - reg1[2])
+    s2 = (reg2[1] - reg2[0]) * (reg2[3] - reg2[2])
 
     return s_in / (s1 + s2 - s_in)
 
 if __name__ == '__main__':
     lists = [
-        [10, 10, 20, 20, 0.9],
-        [11, 12, 21, 22, 0.81],
-        [15, 15, 25, 26, 0.7],
-        [18, 18, 28, 28, 0.85],
-        [20, 20, 30, 30, 0.9],
-        [23, 25, 33, 35, 0.8],
+        [10, 20, 10, 20, 0.9],
+        [11, 21, 12, 22, 0.81],
+        [15, 25, 15, 26, 0.7],
+        [18, 28, 18, 28, 0.85],
+        [20, 30, 20, 30, 0.9],
+        [23, 33, 25, 35, 0.8],
     ]
     threshold = 0.1
 
